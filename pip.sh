b3cd6fd17b392bced73095f4a96403b672c4d294
Tamás Norbert Varga <t.varga@physik.lmu.de>
2022-04-12 16:10:36 +0200

    Update pip.sh

--- pip.sh
+++ pip.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+# pytorch elements
 pip3 install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
+
+# JAX a numpy-like CUDA accelerator
+# Installs the wheel compatible with CUDA 11 and cuDNN 8.2 or newer.
+pip install --upgrade "jax[cuda]" -f https://storage.googleapis.com/jax-releases/jax_releases.html  # Note: wheels only available on linux.
